97e0f7b67185635387c1b8e7864f4c8bf1075dd6
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-01-19 22:08:06 +0100

    Singleton basic structure and adding listener to spaceship

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,42 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputHandler : MonoBehaviour
 {
-    public UnityEngine.Events.UnityEvent spaceshipMove;
-    public UnityEngine.Events.UnityEvent spaceshipShoot;
-    public UnityEngine.Events.UnityEvent spaceshipTurnLeft;
-    public UnityEngine.Events.UnityEvent spaceshipTurnRight;
-
+    public UnityEngine.Events.UnityEvent<Vector2> spaceshipMove;
+    private static InputHandler instance;
+    static InputHandler getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new InputHandler();
+        }
+        return instance;
+    }
+    private void Awake()
+    {
+        spaceshipMove = new UnityEvent<Vector2>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
+            Move(Vector2.up);
             //spaceshipMove.AddListener(); //jak dodac listener z kodu?
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
+            Move(Vector2.left);
 
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
+            Move(Vector2.right);
 
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
         }
+    }
+
+    void Move(Vector2 vector)
+    {
+        spaceshipMove.Invoke(vector);
     }
 }
--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -8,10 +8,14 @@ public class MainScript : MonoBehaviour
 
     public void Awake()
     {
-        if (_shipPrefab == null) throw new UnityException("Ship Prefab is not manually assigned"); 
+        if (_shipPrefab == null) throw new UnityException("Ship Prefab is not manually assigned");
 
         //TODO: Fix not setting object reference
         //GameObject ship = CreateShip(); 
+        GameObject ship = CreateShip();
+        var script = ship.AddComponent<InputHandler>();
+        (script as InputHandler).spaceshipMove.
+             AddListener((ship.GetComponent<SpaceObjectExecutioner>().SpaceObject as Ship).Control);
     }
 
     private GameObject CreateShip()
