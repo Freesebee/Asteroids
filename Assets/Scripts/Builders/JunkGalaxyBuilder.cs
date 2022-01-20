287485bed53d128e4554aecb4e7bd3337bff71fd
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2022-01-20 12:55:23 +0100

    Sample MapBuilder

---
+++ Assets/Scripts/Builders/JunkGalaxyBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+    public class JunkGalaxyBuilder : IMapBuilder
+    {
+        private List<SpaceObject> _map;
+        private ISpaceObjectFactory _spaceObjectFactory;
+
+        public JunkGalaxyBuilder(ISpaceObjectFactory spaceObjectFactory)
+        {
+            _spaceObjectFactory = spaceObjectFactory;
+            _map = new List<SpaceObject>();
+        }
+
+        public void AddPlanet(Vector2 position)
+        {
+            var asteroid = _spaceObjectFactory.CreateAsteroid();
+            asteroid.GameObject.transform.position = position;
+            _map.Add(asteroid);
+        }
+
+        public List<SpaceObject> GetMap() => _map;
+
+        public void ResetMap()
+        {
+            _map = new List<SpaceObject>();
+        }
+}
